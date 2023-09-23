8562a30c4e69d49b263efaa4c8edb28aad88cc44
Herbert Suantak <108845744+HBMods-OFC@users.noreply.github.com>
2023-09-23 15:16:45 +0530

    Add files via upload

--- HBWABot-v3.js
+++ HBWABot-v3.js
@@ -1347,7 +1347,7 @@ case 'alive': case 'panel': case 'list': case 'menu': case 'help': case '?': {
             let { key } = await HBWABotInc.sendMessage(from, {text: '《▒▒▒▒▒▒▒▒▒▒▒▒》0%\n♻️ _Nghak lawk rawh.._'},{quoted:m})
 
             for (let i = 0; i < HBLoadingmenu.length; i++) {await HBWABotInc.sendMessage(from, {text: HBLoadingmenu[i], edit: key },{quoted:m})}
-await menu()
+await Menu()
 }
            break
 case 'allmenu': {
