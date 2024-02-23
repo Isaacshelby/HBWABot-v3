7b0b8debb40a305af124fcd33e82c5d974d2dc12
Isaacshelby <142240645+Isaacshelby@users.noreply.github.com>
2024-02-23 23:14:11 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const birthdayCard = document.querySelector(".birthdayCard");
-  const cardInside = document.querySelector(".cardInside");
-
-  birthdayCard.addEventListener("click", function () {
-    cardInside.classList.toggle("active");
-  });
-});
